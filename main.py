7929c73c055c06292b87f097ca0279070f285880
Abdullah-Alkehaimi <Alkehaimia@gmail.com>
2025-07-07 12:04:39 +1000

    Set FPS to 60 max

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import pygame
 from constants import *
 
-infinite_while = 0
-
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while infinite_while == 0:
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("BLACK")
+            
+        screen.fill("Black")
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
