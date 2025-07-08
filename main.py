f7d308b8298bde4478c58dc5e98c431f30f23bc6
Abdullah-Alkehaimi <Alkehaimia@gmail.com>
2025-07-08 14:52:01 +1000

    changed to use groups instead of calling each object individually

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = updatable, drawable
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -16,8 +20,9 @@ def main():
                 return
             
         screen.fill("Black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
